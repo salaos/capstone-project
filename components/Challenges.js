112db27afa857749f10dfc9e707e15c5884ecff0
Nina Saalfrank <123561210+salaos@users.noreply.github.com>
2023-04-18 10:37:58 +0200

    add checkbox + styling

--- components/Challenges.js
+++ components/Challenges.js
@@ -1,9 +1,14 @@
 // display all the challenges in a grid format
 
-import styled from "styled-components";
 import NoCard from "./NoCard";
 import { useState } from "react";
-import { SmallRoundButton } from "./StyledComponents";
+import {
+  SmallRoundButton,
+  Checkbox,
+  Card,
+  Paragraph,
+  CardContainer,
+} from "./StyledComponents";
 
 export default function ChallengeList({ challenges, onDelete }) {
   const [expandedChallenge, setExpandedChallenge] = useState(null);
@@ -30,6 +35,14 @@ export default function ChallengeList({ challenges, onDelete }) {
           >
             <h2>{expandedChallenge === challenge.id ? "▼" : "▶︎"}</h2>
             <h2>{challenge.title}</h2>
+
+            <Checkbox
+              type="checkbox"
+              id="done"
+              name="done"
+              value="done"
+              checked={challenge.done}
+            ></Checkbox>
 
             {expandedChallenge === challenge.id && (
               <>
@@ -61,49 +74,3 @@ export default function ChallengeList({ challenges, onDelete }) {
     </CardContainer>
   );
 }
-
-const Paragraph = styled.div`
-  margin: 0.8rem 0;
-`;
-
-const CardContainer = styled.div`
-  display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
-  grid-gap: 1rem;
-  justify-items: center;
-  align-items: center;
-  margin: 1rem;
-`;
-
-const Card = styled.div`
-background: var(--cardbackground);
-border: 3px solid var(--tertiarymid);
-border-radius: 20px;
-padding: 1.2rem;
-margin: 0.2rem 0.2rem 0.5rem 0;
-width: 86%;
-height: auto;
-
-align-items: center;
-text-align: left;
-box-shadow: 10px 8px 0px 0 var(--tertiary);
-transition: all 0.3s ease-in-out;
-font-size: 0.8rem;
-h2 {
-  padding: 2px;
-  color: var(--tertiary);
-  opacity: 0.7;
-  text-align: left;
-  display: inline;
-
-}
-
-  &:hover {
-    transform: scale(1.03);
-    box-shadow: 10px 8px 0px 0 var(--primary);
-    h2 {
-      opacity: 1;
-    }
-  }
-}
-`;
